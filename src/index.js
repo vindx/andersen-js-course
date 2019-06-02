c0c07f7f2a46dad6a1f93539a00f9255386fcb30
vindx <kiruha9414@gmail.com>
2019-06-02 15:19:57 +0300

    es task10

--- src/index.js
+++ src/index.js
@@ -24,8 +24,8 @@
 // import { task6Old, task6New } from './es-features/task6';
 // import { task7Old, task7New } from './es-features/task7';
 // import { task8Old, task8New } from './es-features/task8';
- import { task9Old, task9New } from './es-features/task9';
-// import { task10Old, task10New } from './es-features/task10';
+// import { task9Old, task9New } from './es-features/task9';
+import { task10Old, task10New } from './es-features/task10';
 
 // console.log('task1 old', task1Old(10) === 500); // true
 // console.log('task1 new', task1New(10) === 500); // true
@@ -67,12 +67,12 @@
 
 // console.log('=============');
 
- console.log('task9 old', task9Old()); // [true, 1]
- console.log('task9 new', task9New()); // [true, 1]
+// console.log('task9 old', task9Old()); // [true, 1]
+// console.log('task9 new', task9New()); // [true, 1]
 
 // console.log('=============');
 
-// console.log('task10 old', task10Old()); // {name: 'Max', age: 12, color: red}
-// console.log('task10 new', task10New()); // {name: 'Max', age: 12, color: red}
+ console.log('task10 old', task10Old()); // {name: 'Max', age: 12, color: red}
+ console.log('task10 new', task10New()); // {name: 'Max', age: 12, color: red}
 
 // console.log('=============');
