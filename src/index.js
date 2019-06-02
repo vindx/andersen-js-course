c1f6f101ee15962fd88687a888e5c2ef5b3e7d77
vindx <kiruha9414@gmail.com>
2019-06-02 13:17:02 +0300

    es task9

--- src/index.js
+++ src/index.js
@@ -23,8 +23,8 @@
 // import { task5Old, task5New } from './es-features/task5';
 // import { task6Old, task6New } from './es-features/task6';
 // import { task7Old, task7New } from './es-features/task7';
-import { task8Old, task8New } from './es-features/task8';
-// import { task9Old, task9New } from './es-features/task9';
+// import { task8Old, task8New } from './es-features/task8';
+ import { task9Old, task9New } from './es-features/task9';
 // import { task10Old, task10New } from './es-features/task10';
 
 // console.log('task1 old', task1Old(10) === 500); // true
@@ -62,13 +62,13 @@ import { task8Old, task8New } from './es-features/task8';
 
 // console.log('=============');
 
- console.log('task8 old', task8Old()); // [0, 5, 10, 15]
- console.log('task8 new', task8New()); // [0, 5, 10, 15]
+// console.log('task8 old', task8Old()); // [0, 5, 10, 15]
+// console.log('task8 new', task8New()); // [0, 5, 10, 15]
 
 // console.log('=============');
 
-// console.log('task9 old', task9Old()); // [true, 1]
-// console.log('task9 new', task9New()); // [true, 1]
+ console.log('task9 old', task9Old()); // [true, 1]
+ console.log('task9 new', task9New()); // [true, 1]
 
 // console.log('=============');
 
