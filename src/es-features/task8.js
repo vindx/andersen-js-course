529ecaee99b2064edea9a9f80131e51b3b9b8161
vindx <kiruha9414@gmail.com>
2019-06-02 13:07:35 +0300

    es task8

--- src/es-features/task8.js
+++ src/es-features/task8.js
@@ -28,4 +28,20 @@ export function task8Old() {
 // Напишите реализацию функции task8Old на ES6+ ниже этого комментария.
 // При желании, можете использовать стрелочную функцию, вместо обычной
 
-export function task8New() {}
+export function task8New() {
+  let obj = {
+    nums: [0, 2, 5, 10, 15],
+    fives: [],
+    calculateNumsFive() {
+      for (let arg of this.nums) {
+        if (arg % 5 === 0) {
+          this.fives.push(arg);
+        }
+      }
+
+      return this.fives;
+    },
+  };
+
+  return obj.calculateNumsFive();
+}
